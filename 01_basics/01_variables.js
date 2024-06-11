b335605bfce1bf0b542302ce274e9a89ba8d9967
Nirmal7073 <136505934+Nirmal7073@users.noreply.github.com>
2024-06-11 17:39:39 +0000

    datatype

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 1121323
+let userName ="nirmal" 
+var passWord = 1232323
+city = 'valsad'
+let accountstat;
+
+
+// accountId = dfsd NOt allow
+userName = 'pooja'
+pass =121212
+console.log(accountId , userName , passWord ,city ,accountstat)
+
+/* 
+    Perfer not to use var
+    beacause of issue in block scope and function scope
+*/
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict" //treat all js code as newer version
+
+//alert( 3 + 3) we are using node js not a browser
+
+let name = "nirmal"
+let age = 20
+let isLoggeIn = false;
+let pass= null
+
+/* 
+    Primitive data type (immutable menas value that no to changed)
+*/
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => unique (compnent use in react)
+
+/* 
+    Non Primitive data type (mutable data type)
+    object , function ,array
+*/
+
+console.log(typeof name);
+console.log(typeof null);// type object
+console.log(typeof undefined);// type undefined
