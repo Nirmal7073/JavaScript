1eaff54b3a1e019572e824bcb30881cf3393353d
Nirmal7073 <136505934+Nirmal7073@users.noreply.github.com>
2024-06-12 17:28:38 +0000

    conversion of type

---
+++ 01_basics/03_conversionOpration.js
@@ -0,0 +1,46 @@
+
+
+// let scrore = 33
+// let scrore = "33"
+// let scrore = "33abc"
+// let pass = "dfsd"
+// console.log(typeof scrore);
+
+
+// let valueInNumber = Number(scrore);
+
+// console.log(typeof valueInNumber)
+
+// console.log(valueInNumber)
+// let valueInNumb1 = Number(pass)
+// console.log(typeof valueInNumb1);
+// console.log(valueInNumb1);
+
+// convert IN number (value is)
+// "33" => 33
+// "33as" ,"dsf" => NaN
+// true => 1 and false => 0
+// undifined => NaN
+// null => 0 , "" => 0
+
+//boolean
+// let isLoggedIn = 0
+// let isLoggedIn = "nirmal"
+
+// let booleanValue = Boolean(isLoggedIn);
+
+// console.log(booleanValue , typeof booleanValue)
+
+// convert IN boolean  (value is)
+
+// undifined => false 
+// null => false , "" => false
+// 1 => true and 0 => false
+//"nirmal" => ture
+
+// string 
+
+let someNumber = 43
+let stringNumber = String(someNumber);
+
+console.log(typeof stringNumber ,  stringNumber)
