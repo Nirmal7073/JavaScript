add441eebec0720f2a8bc19d38954c8b86566827
nirmal patel <nirmalpatel7073@gmail.com>
2024-06-30 22:48:23 +0530

    object

--- 02_basics/03_object.js
+++ 02_basics/03_object.js
@@ -1,3 +1,10 @@
-//object
-
- 
+//object constructor (singleton // object.create) 
+//object literals
+const JsUser = {
+    name: 'nirmal',
+    age:31,
+    location:'valsad',
+    email:'nirmal@good.com',
+    isLoggedIn:false,
+    LastLoginDays:['monday', 'saturday']
+}
